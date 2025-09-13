977c098365d97e922a0760e7b79138779a1f8709
AshwaniKumar22 <ashwanikumar3022.kkr@gmail.com>
2025-09-13 13:52:07 +0530

    provider-updated

--- provider.tf
+++ provider.tf
@@ -1,13 +1,13 @@
 terraform {
-    required_providers {
-      azurerm = {
-        source="hashicorp/azurerm"
-        version = "3.116.0"
-      }
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.44.0"
     }
+  }
     backend "azurerm" {
-      resource_group_name = "Pipeline-rg"
-      storage_account_name = "pipelinestrg"
+      resource_group_name = "practicedev-rg"
+      storage_account_name = "practicedevstrg"
       container_name = "testcontainer"
       key="test.terraform.tfstate"
     }
@@ -16,4 +16,5 @@ terraform {
 
 provider "azurerm"{
     features {}
+    subscription_id = "16648c27-24de-48bf-b2d6-cc829eea5412"
 }
